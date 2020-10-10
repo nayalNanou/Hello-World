279a6eff5bf99eac44f947ab87b3ada9b5a12901
nayalNanou <nayal.christophcolomb@gmail.com>
2020-10-10 10:11:52 +0200

    Ajout ( img1 ) dans ( index.php )

--- index.php
+++ index.php
@@ -10,6 +10,12 @@
 	<body>
 		<h1>Titre principale de la page</h1>
 
+		<main id="page">
+			<p>
+				<img src="https://media.giphy.com/media/HZboJ5Pkti9k4/source.gif" class="imageType1" alt="pic1" />
+			</p>
+		</main>
+
 		<script src="script.jps"></script>
 	</body>
 </html>
