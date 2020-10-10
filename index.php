dca7133a564b23741766c3fbda8a6f9ca420d70e
nayalNanou <nayal.christophcolomb@gmail.com>
2020-10-10 10:19:53 +0200

    Ajout ( texte n°1 ) ( index.php )

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 		<main id="page">
 			<p>
 				<img src="https://media.giphy.com/media/HZboJ5Pkti9k4/source.gif" class="imageType1" alt="pic1" />
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet scelerisque tellus, eget dictum turpis. Sed porttitor at arcu vel mattis. Donec fermentum, eros a dictum efficitur, orci magna aliquam tortor, id placerat nisl nibh pretium libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin consequat est nisi, sed convallis nunc pulvinar eget. Maecenas orci ante, vulputate nec turpis ut, scelerisque aliquam elit. Phasellus tempor urna varius, dictum sapien sit amet, vestibulum ex.
 			</p>
 		</main>
 
