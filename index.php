38d9eb2c89f858232c86547efb3439b77f733708
nayalNanou <nayal.christophcolomb@gmail.com>
2020-10-10 12:52:49 +0200

    Ajout ( img4 ) Image animée Type animation : ( disparition réapparition )

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet sceleri
 			<p>
 				<img src="https://4.bp.blogspot.com/-vmmGa0jq0TM/UukXTF0NmBI/AAAAAAAACfg/Qx31p7pVC48/s1600/Dance+020.gif" class="rotateImage" id="rotateImage1" />
 				<img src="https://media.tenor.com/images/1d4d4864c02aa7026b18b76479ad6bc3/tenor.gif" class="rotateImage" id="rotateImage2" />
+				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/603f335b-75d7-4b0b-b988-857ca4dee307/d6arol8-f33fc832-cac0-42d0-846d-d32b53e647d6.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi82MDNmMzM1Yi03NWQ3LTRiMGItYjk4OC04NTdjYTRkZWUzMDcvZDZhcm9sOC1mMzNmYzgzMi1jYWMwLTQyZDAtODQ2ZC1kMzJiNTNlNjQ3ZDYuZ2lmIn1dXX0.lfIuxKF7B2jCQH3mjBRPB0yPbajlG1ej1ngj4zz_kbc" class="disappearReappearImage" id="disappearReappearImage1" />
 			</p>
 		</main>
 
--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let rotateImage1 = document.getElementById('rotateImage1'),
 let degreeRotationImage1 = 0,
     degreeRotationImage2 = 0;
 
+let disappearReappearImage1 = document.getElementById('disappearReappearImage1');
+let opacityImage1 = 1, isImage1Visible = true;
+
 function animationRotation(element, degreeRotation, angleRotation) {
 	if (angleRotation == 'positive') {
 		degreeRotation += 0.5;
@@ -31,3 +34,35 @@ animationRotation(rotateImage1, degreeRotationImage1, 'positive');
 animationRotation(rotateImage2, degreeRotationImage2, 'negative');
 
 
+function disappearReappearAnimation(element, opacityElement, isImageVisible) {
+	let animationPause = false;
+
+	if (opacityElement >= 1) {
+		isImageVisible = true;
+		animationPause = true;
+	} else if (opacityElement <= 0) {
+		isImageVisible = false;
+	}
+
+	if (isImageVisible) {
+		opacityElement -= 0.01;
+	} else {
+		opacityElement += 0.01;
+	}
+
+	element.style.opacity = opacityElement;
+
+	if (!animationPause) {
+		window.requestAnimationFrame(function() {
+			disappearReappearAnimation(element, opacityElement, isImageVisible);
+		});
+	} else {
+		setTimeout(function() {
+			window.requestAnimationFrame(function() {
+				disappearReappearAnimation(element, opacityElement, isImageVisible);
+			});
+		}, 2000);
+	}
+}
+
+disappearReappearAnimation(disappearReappearImage1, opacityImage1, isImage1Visible);
