00302bdfb3b126323fbe5b8f6a7ace32983ee452
nayalNanou <nayal.christophcolomb@gmail.com>
2020-10-10 17:38:28 +0200

    Ajout d'un ( header ) : En-tête

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
 	</head>
 
 	<body>
+		<header>
+			<p>Proin hendrerit tincidunt</p>
+			<div class="moon"></div>
+		</header>
+
 		<h1>Titre principale de la page</h1>
 
 		<main id="page">
@@ -16,7 +21,7 @@
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet scelerisque tellus, eget dictum turpis. Sed porttitor at arcu vel mattis. Donec fermentum, eros a dictum efficitur, orci magna aliquam tortor, id placerat nisl nibh pretium libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin consequat est nisi, sed convallis nunc pulvinar eget. Maecenas orci ante, vulputate nec turpis ut, scelerisque aliquam elit. Phasellus tempor urna varius, dictum sapien sit amet, vestibulum ex.
 			</p>
 
-			<p>
+			<p style="clear: both;">
 				<img src="https://4.bp.blogspot.com/-vmmGa0jq0TM/UukXTF0NmBI/AAAAAAAACfg/Qx31p7pVC48/s1600/Dance+020.gif" class="rotateImage" id="rotateImage1" alt="pic2" />
 				<img src="https://media.tenor.com/images/1d4d4864c02aa7026b18b76479ad6bc3/tenor.gif" class="rotateImage" id="rotateImage2" alt="pic3" />
 				<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/603f335b-75d7-4b0b-b988-857ca4dee307/d6arol8-f33fc832-cac0-42d0-846d-d32b53e647d6.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi82MDNmMzM1Yi03NWQ3LTRiMGItYjk4OC04NTdjYTRkZWUzMDcvZDZhcm9sOC1mMzNmYzgzMi1jYWMwLTQyZDAtODQ2ZC1kMzJiNTNlNjQ3ZDYuZ2lmIn1dXX0.lfIuxKF7B2jCQH3mjBRPB0yPbajlG1ej1ngj4zz_kbc" class="disappearReappearImage" id="disappearReappearImage1" alt="pic4" />
